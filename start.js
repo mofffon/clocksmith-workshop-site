8b36990f6f6b2dbf1da8a9ea00d52851ad8b27fa
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2022-09-27 17:03:48 +0200

    Adding the link fix so no js is used.

--- start.js
+++
@@ -1,8 +0,0 @@
-
-const start = () =>{
-    document.querySelectorAll(".directions").forEach(
-        elem => elem.addEventListener("click", 
-            (e) => { e.preventDefault(); document.forms[0].submit() }, false));
-}
-
-window.addEventListener("load", start, false);
